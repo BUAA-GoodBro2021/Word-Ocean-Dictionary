cdd2fd06d860738e1f5bd0c990bd908e4896062a
FL-Rover <2947653177@qq.com>
2022-05-05 11:19:53 +0800

    feat: add part of mainactivity

--- app/src/main/java/com/example/word_ocean_dictionary/MainActivity.java
+++ app/src/main/java/com/example/word_ocean_dictionary/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.word_ocean_dictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
     }
 }
