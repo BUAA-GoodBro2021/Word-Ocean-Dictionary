b02fa6489be2d2f6930383f60fd21e69a39a02cb
暗中观察|.･｀) <2868470542@qq.com>
2022-05-16 11:33:46 +0800

    feat : finish search left and right adapter  .java

---
+++ app/src/main/java/com/example/word_ocean_dictionary/adapter/SearchRightAdapter.java
@@ -0,0 +1,67 @@
+package com.example.word_ocean_dictionary.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.word_ocean_dictionary.R;
+import com.example.word_ocean_dictionary.bean.PinBuWordBean;
+
+import java.util.List;
+
+/**
+ *  拼音查询，部首查询页面右侧PullToRrefreshGriView的适配器
+ * */
+public class SearchRightAdapter extends BaseAdapter {
+    Context context;
+    List<PinBuWordBean.ResultBean.ListBean> mDatas;
+    LayoutInflater inflater;
+    public SearchRightAdapter(Context context, List<PinBuWordBean.ResultBean.ListBean> mDatas) {
+        this.context = context;
+        this.mDatas = mDatas;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mDatas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDatas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_search_pgv,null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        PinBuWordBean.ResultBean.ListBean bean = mDatas.get(position);
+        String zi = bean.getZi();
+        holder.tv.setText(zi);
+        return convertView;
+    }
+
+    static class ViewHolder{
+        TextView tv;
+        public ViewHolder(View view){
+            tv = view.findViewById(R.id.item_sgv_tv);
+        }
+    }
+}
