2a463b73f61f9fcf20aa4d4536319e6f0a100229
暗中观察|.･｀) <2868470542@qq.com>
2022-05-16 18:26:31 +0800

    feat : finish the framework of all dict

---
+++ refresh_library/build/generated/source/buildConfig/release/com/handmark/pulltorefresh/library/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.handmark.pulltorefresh.library;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.handmark.pulltorefresh.library";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.handmark.pulltorefresh.library";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2110;
+  public static final String VERSION_NAME = "2.1.1";
+}
